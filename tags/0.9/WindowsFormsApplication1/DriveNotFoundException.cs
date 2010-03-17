2662633f82b5b37ad7e3a8b12c9773262c70a578
jdwxjdwx <jdwxjdwx@ef77fb6a-21da-11df-8f40-0d449d17a094>
2010-03-17 12:06:49 +0000

    Created tag for release 0.9.

---
+++ tags/0.9/WindowsFormsApplication1/DriveNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameAnywhere
+{
+    class DriveNotFoundException : System.ApplicationException
+    {
+        public DriveNotFoundException() { }
+        public DriveNotFoundException(String message) { }
+        public DriveNotFoundException(String message, System.Exception inner) { }
+    }
+}
---
+++ tags/0.9/WindowsFormsApplication1/SyncFolderFileDiscrepancyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameAnywhere
+{
+    class SyncFolderFileDiscrepancyException : System.ApplicationException
+    {
+        public SyncFolderFileDiscrepancyException() { }
+        public SyncFolderFileDiscrepancyException(String message) { }
+        public SyncFolderFileDiscrepancyException(String message, System.Exception inner) { }
+    }
+}
